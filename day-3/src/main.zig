4801a3cf6ca68bcd425514656c30867601df7b1a
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2024-12-03 23:13:25 +0200

    day 3 part 2

--- day-3/src/main.zig
+++ day-3/src/main.zig
@@ -1,3 +1,6 @@
+// I could have used regex engine from c... buuuut i was too lazy to read it's complicated manual,
+// so why not to build my own little parser? That should be easier, right?
+// This parser is very broken, but it solved both parts... Sorry future me, deal with it
 const std = @import("std");
 
 const print = std.debug.print;
@@ -5,15 +8,24 @@ const print = std.debug.print;
 const State = struct {
     const Level = enum {
         Idle,
+
+        D,
+        O,
+        N,
+        Quot,
+        T,
+
         M,
         U,
         L,
-        BO,
-        D,
+        Digit,
         C,
+
+        BO,
         BC,
     };
 
+    enabled: bool,
     level: Level,
     buf: [3]u8,
     currentBufIdx: u8,
@@ -44,17 +56,37 @@ fn parse(state: *State, input: []const u8) !u32 {
 
     while (i < input.len) : (i += 1) {
         const c = input[i];
-        // print("{c} - {any}\n", .{ c, state });
 
         switch (state.level) {
-            .Idle => cmpAndSetState(state, .M, c, 'm'),
+            .Idle => {
+                switch (c) {
+                    'm' => cmpAndSetState(state, .M, c, 'm'),
+                    'd' => cmpAndSetState(state, .D, c, 'd'),
+                    else => resetState(state),
+                }
+            },
+
+            .D => cmpAndSetState(state, .O, c, 'o'),
+            .O => {
+                switch (c) {
+                    '(' => state.level = .BO,
+                    'n' => state.level = .N,
+                    else => resetState(state),
+                }
+            },
+
+            .N => cmpAndSetState(state, .Quot, c, '\''),
+            .Quot => cmpAndSetState(state, .T, c, 't'),
+            .T => cmpAndSetState(state, .BO, c, '('),
+
             .M => cmpAndSetState(state, .U, c, 'u'),
             .U => cmpAndSetState(state, .L, c, 'l'),
             .L => cmpAndSetState(state, .BO, c, '('),
+
             .BO => {
                 switch (c) {
                     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
-                        state.*.level = .D;
+                        state.*.level = .Digit;
 
                         if (state.currentBufIdx < 3) {
                             state.*.buf[state.currentBufIdx] = c;
@@ -63,15 +95,19 @@ fn parse(state: *State, input: []const u8) !u32 {
                             resetState(state);
                         }
                     },
+                    ')' => {
+                        i -= 1;
+                        state.level = .BC;
+                    },
                     else => {
                         resetState(state);
                     },
                 }
             },
-            .D => {
+            .Digit => {
                 switch (c) {
                     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
-                        state.*.level = .D;
+                        state.*.level = .Digit;
 
                         if (state.currentBufIdx < 3) {
                             state.*.buf[state.currentBufIdx] = c;
@@ -106,7 +142,7 @@ fn parse(state: *State, input: []const u8) !u32 {
                         resetState(state);
 
                         state.*.num = num;
-                        state.*.level = .D;
+                        state.*.level = .Digit;
 
                         // print("parsed first num: {}\n", .{num});
                     } else |_| {
@@ -120,7 +156,11 @@ fn parse(state: *State, input: []const u8) !u32 {
                 }
             },
             .BC => {
-                if (state.currentBufIdx > 0) {
+                const prevChar = input[i - 2];
+
+                if (prevChar == 'o' or prevChar == 't') {
+                    state.enabled = if (prevChar == 'o') true else false;
+                } else if (state.currentBufIdx > 0 and state.enabled) {
                     const numStr = state.buf[0..state.currentBufIdx];
 
                     if (std.fmt.parseInt(u32, numStr, 10)) |num| {
@@ -144,6 +184,7 @@ fn parse(state: *State, input: []const u8) !u32 {
 
 pub fn main() !void {
     var state: State = .{
+        .enabled = true,
         .level = .Idle,
         .buf = undefined,
         .currentBufIdx = 0,
@@ -151,6 +192,12 @@ pub fn main() !void {
     };
 
     var sum: u32 = 0;
+
+    // const input =
+    //     \\)mul(542,816):don't()select()from()#(;!select()mul(541,668)
+    // ;
+    //
+    // sum += try parse(&state, input);
 
     const file = try std.fs.cwd().openFile("input", .{});
     defer file.close();
